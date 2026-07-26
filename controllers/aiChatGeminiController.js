cdfe818fb641d9a312bfc2ee1eb0803f36827eb2
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2026-07-27 03:02:39 +0530

    function name change

---
+++ controllers/aiChatGeminiController.js
@@ -0,0 +1,1 @@
+export const postChat = async (req, res, apiKey, model, system_prompt) => {};
--- routes/aiChatRoute.js
+++ routes/aiChatRoute.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { postChat } from "../controllers/aiChatOpenrouterController.js";
+import { postChat as postChatOpenrouter } from "../controllers/aiChatOpenrouterController.js";
+import { postChat as postChatGemini } from "../controllers/aiChatGeminiController.js";
 
 const router = Router();
 
@@ -7,7 +8,14 @@ router.post("/openrouter", async (req, res) => {
   const apiKey = req.headers["x-api-key"] || req.body.apiKey || "";
   const model = req.body.model || "";
   const system_prompt = req.body.system_prompt || "";
-  await postChat(req, res, apiKey, model, system_prompt);
+  await postChatOpenrouter(req, res, apiKey, model, system_prompt);
+});
+
+router.post("/gemini", async (req, res) => {
+  const apiKey = req.headers["x-api-key"] || req.body.apiKey || "";
+  const model = req.body.model || "";
+  const system_prompt = req.body.system_prompt || "";
+  await postChatGemini(req, res, apiKey, model, system_prompt);
 });
 
 export default router;
